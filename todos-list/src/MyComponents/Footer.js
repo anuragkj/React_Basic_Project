e1a405734ecfd733a0896fe8b8201bae6039bdd7
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2023-01-18 16:22:29 +0530

    Adding footer

--- todos-list/src/MyComponents/Footer.js
+++ todos-list/src/MyComponents/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 export const Footer = () => {
+  let footerStyle = {
+    position: "absolute",
+    bottom: "0vh",
+    width: "100%",
+  }
   return (
-    <div>
-        Footer works
-    </div>
+    <footer className='bg-dark text-light text-center py-3 px-3' style={footerStyle}>
+        Copyright &copy; TodoList.com
+    </footer>
   )
 }
--- todos-list/src/MyComponents/Todos.js
+++ todos-list/src/MyComponents/Todos.js
@@ -5,9 +5,11 @@ export const Todos = (props) => {
   return (
     <div className="container">
       <h3 className='text-center my-3'>Todos List</h3>
-      {props.todos.map((todo) =>{
-        return (<TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>)
-      })}
+      {props.todos.length === 0? <h3>No Todos Available</h3> : 
+      props.todos.map((todo) =>{
+        return <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>
+      })
+    }
       
     </div>
   )
