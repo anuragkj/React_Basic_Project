fe7ca54f28d7bcaa7e470f619cdae4d435dbf654
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2023-01-18 14:19:23 +0530

    Added loop

--- todos-list/src/MyComponents/Todos.js
+++ todos-list/src/MyComponents/Todos.js
@@ -5,7 +5,10 @@ export const Todos = (props) => {
   return (
     <div className="container">
       <h3 className='text-center my-3'>Todos List</h3>
-      <TodoItem todo={props.todos[0]}/>
+      {props.todos.map((todo) =>{
+        return (<TodoItem todo={todo}/>)
+      })}
+      
     </div>
   )
 }
